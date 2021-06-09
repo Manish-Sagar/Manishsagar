e5ea73212b033cac54a5e5e47c251ce045ddc71f
Manish Sagar <83937898+Manish-Sagar@users.noreply.github.com>
2021-06-09 20:59:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(`.navbar`).addClass("sticky");
+    } else {
+      $(`.navbar`).removeClass("sticky");
+    }
+    if (this.scrollY > 100) {
+      $(`.scroll-up-btn`).addClass("show");
+    } else {
+      $(`.scroll-up-btn`).removeClass("show");
+    }
+  });
+  // slide up
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+  // toggle menu/bar
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
+var typed = new Typed(".typing", {
+  strings: [
+    "Student",
+    "Web Developer",
+    "Digital  Marketer",
+    "Social Media Handler",
+  ],
+  typespeed: 100,
+  backspeed: 60,
+  loop: 0,
+});
+var typed = new Typed(".typing-1", {
+  strings: [
+    "Student",
+    "Web Developer",
+    "Digital  Marketer",
+    "Social Media Handler",
+  ],
+  typespeed: 100,
+  backspeed: 60,
+  loop: 0,
+});
